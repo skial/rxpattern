9f9944912be6e8c46b4e7c473d0fafd4465b0b46
Brendon <skial.bainn@gmail.com>
2019-05-13 13:47:38 +0100

    Minor error formatting.

--- rxpattern/RxErrors.hx
+++ rxpattern/RxErrors.hx
@@ -1,8 +1,8 @@
 package rxpattern;
 
 enum abstract RxErrors(String) to String {
-    var Char_NotSingleCharacter = "rxpattern.RxPattern.Char: not a single character";
-    var CharSet_NotCodePoint = "rxpattern.CharSet: not a single code point";
-    var Unicode_InvalidEscape = "Invalid unicode escape sequence";
+    var Char_NotSingleCharacter = "rxpattern.RxPattern.Char: Not a single character.";
+    var CharSet_NotCodePoint = "rxpattern.CharSet: Not a single code point.";
+    var Unicode_InvalidEscape = "Invalid Unicode escape sequence.";
     var Unicode_GreaterThanBMP = "This platform does not support Unicode escape beyond BMP.";
 }
